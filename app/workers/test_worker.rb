9a05fe1a71ecc1e2d414e4abbf3eb9d692845321
Nan Zhong <nan@nine27.com>
2013-10-21 10:48:24 -0400

    Fix typos

--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -3,7 +3,7 @@ class TestWorker
 
   def perform(solution_id)
     @solution = Solution.find(solution_id)
-    `echo solveme | sudo -S docker run -u tunner solve/runner ruby /home/runner/runner.rb #{@solution.problem.id} #{@solution.id}`
+    puts `echo solveme | sudo -S docker run -u runner solve/runner ruby /home/runner/runner.rb #{@solution.problem.id} #{@solution.id}`
     #`ruby /Users/nan/src/solve/runner/runner.rb #{@solution.problem.id} #{@solution.id}`
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,5 @@ Language.create([
   { name: 'ruby 2.0.0p247 (2013-06-27 revision 41674) [x86_64-linux]', key: 'ruby', command: 'ruby _file_ _input_'},
   { name: 'Python 2.7.3', key: 'python2', command: 'python2 _file_ _input_'},
   { name: 'Python 3.2.3', key: 'python3', command: 'python3 _file_ _input_'},
-  { name: 'HP 5.3.10-1ubuntu3 with Suhosin-Patch (cli) (built: Apr 11 2012 17:25:33)', key: 'php', command: 'php _file_ _input_'}
+  { name: 'PHP 5.3.10-1ubuntu3 with Suhosin-Patch (cli) (built: Apr 11 2012 17:25:33)', key: 'php', command: 'php _file_ _input_'}
 ])
