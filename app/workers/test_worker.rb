b8a949c5b8eb59d0e286996f7e806530379608df
Nan Zhong <nan@nine27.com>
2013-10-21 09:55:07 -0400

    Update docker runnner command

--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -3,7 +3,7 @@ class TestWorker
 
   def perform(solution_id)
     @solution = Solution.find(solution_id)
-    `docker run -u tunner solve/runner ruby /home/runner/runner.rb #{@solution.problem.id} #{@solution.id}`
+    `echo solveme | sudo -S docker run -u tunner solve/runner ruby /home/runner/runner.rb #{@solution.problem.id} #{@solution.id}`
     #`ruby /Users/nan/src/solve/runner/runner.rb #{@solution.problem.id} #{@solution.id}`
   end
 end
